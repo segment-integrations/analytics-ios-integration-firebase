b6b213c9a8a5d3aa8500ef2091d8e3d8943f02b9
Cihat Gündüz <commits@cihatguenduez.de>
2021-01-29 16:29:23 +0100

    Add basic Swift package manifest

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "SegmentFirebase",
+    platforms: [.macOS(.v10_10), .iOS(.v8)],
+    products: [.library(name: "SegmentFirebase", targets: ["SegmentFirebase"])],
+    dependencies: [
+      .package(name: "Segment", url: "https://github.com/segmentio/analytics-ios.git", from: "4.1.2"),
+      .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", from: "7.5.0"),
+    ],
+    targets: [
+        .target(
+            name: "SegmentFirebase",
+            dependencies: [
+              "Segment",
+              .product(name: "FirebaseAnalytics", package: "Firebase"),
+            ],
+            path: "Segment-Firebase/Classes",
+            publicHeadersPath: "include"
+        )
+    ]
+)
