62774f98a37eca656b25635af13fde9a620c7d83
Rick Pasveer <rick@booqable.com>
2021-07-07 11:05:39 +0200

    Update minimum ios deployment to match firebase minimum

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "SegmentFirebase",
-    platforms: [.iOS(.v10)],
+    platforms: [.iOS(.v11)],
     products: [.library(name: "SegmentFirebase", targets: ["SegmentFirebase"])],
     dependencies: [
       .package(name: "Segment", url: "https://github.com/segmentio/analytics-ios.git", from: "4.1.3"),
