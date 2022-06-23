61847b0dfcb6392187e13cb54db7dc97b3590c96
Michael Grosse Huelsewiesche <mihuelsewiesche@twilio.com>
2022-06-22 21:33:43 -0400

    Updating some old deprecations that finally went away, with a minor version bump that just released

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     products: [.library(name: "SegmentFirebase", targets: ["SegmentFirebase"])],
     dependencies: [
       .package(name: "Segment", url: "https://github.com/segmentio/analytics-ios.git", from: "4.1.6"),
-      .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", from: "9.1.0"),
+      .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", from: "9.2.0"),
     ],
     targets: [
         .target(
